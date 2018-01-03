a4f850a53f9655aa93ade98e5b16363de969d102
Hines Kao <hineskao@gmail.com>
2018-01-03 11:45:12 -0800

    Utilized Webpack and Babel in order to import and export js files and writing modern Javascript

---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,21 @@
+import $ from "jquery";
+
+class MobileMenu {
+  constructor(){
+    this.siteHeader = $(".site-header");
+    this.menuIcon = $(".site-header__menu-icon");
+    this.menuContent = $(".site-header__menu-content");
+    this.events();
+  }
+
+  events(){
+    this.menuIcon.click(this.toggleTheMenu.bind(this));
+  }
+
+  toggleTheMenu(){
+    this.siteHeader.toggleClass("site-header__is-expanded");
+    this.menuContent.toggleClass("site-header__menu-content__is-visible");
+  }
+}
+
+export default MobileMenu;
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,12 +5,13 @@ var
   cssvars = require("postcss-simple-vars"),
   cssnested = require("postcss-nested"),
   cssImport = require("postcss-import"),
-  mixins = require("postcss-mixins");
+  mixins = require("postcss-mixins"),
+  hexrgba = require("postcss-hexrgba");
 
 
 gulp.task("styles", function(){
   return gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, mixins, cssvars, cssnested, autoprefixer,]))
+    .pipe(postcss([cssImport, mixins, cssvars, cssnested, hexrgba, autoprefixer]))
     .on("error", function(errorInfo){
       console.log(errorInfo.toString());
       this.emit("end");
