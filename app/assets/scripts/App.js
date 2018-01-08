14aaa897538c61a43833a0a621d4b569c8d1ca38
Hines Kao <hineskao@gmail.com>
2018-01-08 11:16:25 -0800

    reveal on scroll completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
-import MobileMenu from "./modules/MobileMenu"
+import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/revealOnScroll";
+import $ from "jquery";
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "65%");
